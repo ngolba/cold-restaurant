6a859917538e42abd48fe4ea2ab780104bf808da
tall karol <karolzbuczek@gmail.com>
2018-10-23 20:48:18 -0400

    added stuff

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const PORT = process.env.PORT || 7100;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-app.listen(PORT, () => console.log(`Index listening on PORT ${PORT}!`))
+var reservations = [{}]
+var waitlist = [{}]
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -21,3 +20,20 @@ app.get("/reserve", function (req, res) {
 app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
+
+app.get("/api/tables", function(req, res) {
+  return res.json(reservations)
+});
+
+app.get("/api/waitlist", function(req,res) {
+  return res.json(waitlist)
+});
+
+app.post("api/tables", function(req, res) {
+  newReso = req.body;
+  console.log(newReso);
+  reservations.push(newReso);
+  res.json(newReso)
+});
+
+app.listen(PORT, () => console.log(`Index listening on PORT ${PORT}!`))
